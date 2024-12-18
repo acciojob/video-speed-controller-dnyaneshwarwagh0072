0cff0a3f89306dfeef2c43abfe3e103ce2d248a8
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-12-18 19:59:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const player = document.querySelector('.player');
-    const video = player.querySelector('.viewer');
-    const toggle = player.querySelector('.toggle');
-    const progress = player.querySelector('.progress');
-    const progressFilled = player.querySelector('.progress__filled');
-    const skipButtons = player.querySelectorAll('[data-skip]');
-    const volumeSlider = player.querySelector('input[name="volume"]');
-    const playbackSpeedSlider = player.querySelector('input[name="playbackSpeed"]');
+const inputs = document.querySelectorAll('.controls input');
 
-    // Add event listeners and functions here
-    function togglePlay() {
-        if (video.paused) {
-            video.play();
-        } else {
-            video.pause();
-        }
+    function handleUpdate() {
+      const suffix = this.dataset.sizing || '';
+      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     }
 
-    function updateButton() {
-        const icon = video.paused ? '►' : '❚ ❚';
-        toggle.textContent = icon;
-    }
-
-    function skip() {
-        video.currentTime += parseFloat(this.dataset.skip);
-    }
-
-    function handleVolume() {
-        video.volume = volumeSlider.value;
-    }
-
-    function handlePlaybackSpeed() {
-        video.playbackRate = playbackSpeedSlider.value;
-    }
-
-    function updateProgress() {
-        const percent = (video.currentTime / video.duration) * 100;
-        progressFilled.style.width = `${percent}%`;
-    }
-
-    function seek(e) {
-        const seekTime = (e.offsetX / progress.offsetWidth) * video.duration;
-        video.currentTime = seekTime;
-    }
-
-    toggle.addEventListener('click', togglePlay);
-    video.addEventListener('play', updateButton);
-    video.addEventListener('pause', updateButton);
-    skipButtons.forEach(button => button.addEventListener('click', skip));
-    volumeSlider.addEventListener('input', handleVolume);
-    playbackSpeedSlider.addEventListener('input', handlePlaybackSpeed);
-    video.addEventListener('timeupdate', updateProgress);
-    progress.addEventListener('click', seek);
-});
+    inputs.forEach(input => input.addEventListener('change', handleUpdate));
+    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
